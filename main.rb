40d49addf6f4c65cb0cbdee5a4f2f6dc0fe029c6
Joe <joedravarol@gmail.com>
2019-04-29 16:41:17 +0100

    Create Board class & add update board method

--- main.rb
+++ main.rb
@@ -5,10 +5,7 @@
 # Build your game, taking care to not share information between classes any more than you 
 # have to.
 
-# [[1,2,3], [4,5,6], [7,8,9]]
 # [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [7,5,3]]
-# row = | 
-# col = --+---+--
 
 # 1. Display instuctions
 # 2. Ask player if they would like to choose to be 'X' or 'O'
@@ -21,3 +18,35 @@
 # Possible Class - Board, Game, Players, Player1 < Players, Player2 < Players
 # Possible methods - check win condition, update board, deal with player move data
 # Possible methods - Choosing input type ('X', 'O'), Update board data, 
+
+class Board
+  @@cells = [[1,2,3], [4,5,6], [7,8,9]]
+
+  def initialize
+    update_board
+  end
+
+  def update_board(cells = @@cells)
+    i = 0
+    while i < cells.length
+      cells[i].each_with_index do |element, index|
+
+        # Element NOT last position?
+        if index != cells[i].length - 1
+          print " #{element} |"
+        # Last inner array?
+        elsif cells[i] == cells.last 
+          # Last element of inner array?
+          if index == cells[i].length - 1
+            puts " #{element}" 
+          end
+        else
+          puts " #{element}"
+          puts "---+---+---"
+        end
+        
+      end
+      i += 1
+    end 
+  end
+end
